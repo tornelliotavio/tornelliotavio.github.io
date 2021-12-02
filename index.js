3c44a2a3ea7578daddd8278c1b98e625e15b9ccc
Otavio Tornelli Duarte <tornelli.otavio20@gmail.com>
2021-12-01 21:26:35 -0300

    some changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let currentCategory = 'all';
+
 for (let item in items) {
   let arr = items[item].name.toLowerCase().split(' ');
   for (var i = 0; i < arr.length; i++) {
@@ -6,7 +8,21 @@ for (let item in items) {
   let name = arr.join(' ').replace('Kg', 'KG');
   items[item].name = name;
 }
-categoryFilter('all');
+
+let finalCategories = '';
+let finalCategoriesMobile = '';
+
+for (let cat in itemCategories) {
+  finalCategories += getAddToCategory(cat);
+  finalCategoriesMobile += getAddToCategory(cat, true);
+}
+
+let categoryHTML = document.getElementById('list-group');
+categoryHTML.innerHTML = finalCategories;
+let categoryMobileHTML = document.getElementById('list-groupMobile');
+categoryMobileHTML.innerHTML = finalCategoriesMobile;
+
+categoryFilter(currentCategory);
 
 function addToItemList(itemListHTML, item) {
   itemListHTML.innerHTML +=
@@ -17,16 +33,36 @@ function addToItemList(itemListHTML, item) {
     $('[data-bs-toggle="tooltip"]').tooltip();
   });
 }
+function getAddToCategory(cat, mobile = false) {
+  return (
+    `<li id="` +
+    (cat + (mobile ? 'M' : '')) +
+    `" class="list-group-item" onclick="categoryFilter('` +
+    cat +
+    `')">
+    <a>` +
+    itemCategories[cat] +
+    `</a>
+  </li>`
+  );
+}
 
 function getAddToItemListHtml(item) {
   return `<div class="col-6 col-md-6 col-lg-4 col-xl-3 cardContainer">
             <div class="card">
-              <img class="card-img-top" src="${item.image}" alt="Card image cap">
+              <img class="card-img-top" src="${
+                item.image
+              }" alt="Card image cap">
             </div>
             <div class="card-body">
-              <h4 class="card-title" data-bs-toggle="tooltip" data-bs-placement="top" title="${item.name}">
+              <h4 style="margin-bottom: 0px;" class="card-title" data-bs-toggle="tooltip" data-bs-placement="top" title="${
+                item.name
+              }">
                 <a>${item.name}</a>
               </h4>
+              <div class="item-footer text-muted">
+              Categoria: ${itemCategories[item.category]}
+            </div>
             </div>
           </div>`;
 }
@@ -35,32 +71,55 @@ function searchFilter() {
   let searchFilterVal = $('#searchFilterInput').val().toLowerCase();
 
   let itemListHTML = document.getElementById('item-list');
-  itemListHTML.innerHTML = '';
+  let itemListTitleHTML = document.getElementById('item-list-title');
 
-  const targetHtml = items
-    .filter((i) => i.name.toLowerCase().indexOf(searchFilterVal) != -1)
-    .reduce((acc, curr) => (acc += getAddToItemListHtml(curr)), ``);
-  itemListHTML.innerHTML = targetHtml;
-  $(function () {
-    $('[data-bs-toggle="tooltip"]').tooltip();
-  });
+  itemListHTML.innerHTML = '';
+  itemListTitleHTML.innerHTML =
+    searchFilterVal.length < 1
+      ? itemCategories[currentCategory]
+      : searchFilterVal + ' em ' + itemCategories[currentCategory];
+
+  if (searchFilterVal.length < 1) {
+    categoryFilter(currentCategory);
+  } else {
+    let currentCatItems = items.filter(
+      (i) => i.category == currentCategory || currentCategory == 'all',
+    );
+    console.log(currentCatItems);
+    const targetHtml = currentCatItems
+      .filter((i) => i.name.toLowerCase().indexOf(searchFilterVal) != -1)
+      .reduce((acc, curr) => (acc += getAddToItemListHtml(curr)), ``);
+    itemListHTML.innerHTML = targetHtml;
+    $(function () {
+      $('[data-bs-toggle="tooltip"]').tooltip();
+    });
+  }
 }
 
 function categoryFilter(category) {
+  const lastCategory = currentCategory;
+
   let itemListHTML = document.getElementById('item-list');
+  let itemListTitleHTML = document.getElementById('item-list-title');
   itemListHTML.innerHTML = ``;
+  $('#searchFilterInput').val('');
 
   const targetHtml = (
     category !== 'all' ? items.filter((i) => i.category === category) : items
   ).reduce((acc, curr) => (acc += getAddToItemListHtml(curr)), ``);
 
   itemListHTML.innerHTML = targetHtml;
+  if (itemCategories[category]) {
+    currentCategory = category;
+  }
+  itemListTitleHTML.innerHTML = itemCategories[category] || '';
+
   $(function () {
     $('[data-bs-toggle="tooltip"]').tooltip();
   });
-  // for (let item of items) {
-  //   if (item.category === category || category === 'all') {
-  //     addToItemList(itemListHTML, item);
-  //   }
-  // }
+
+  $('#' + lastCategory).removeClass('active');
+  $('#' + category).addClass('active');
+  $('#' + lastCategory + 'M').removeClass('active');
+  $('#' + category + 'M').addClass('active');
 }
